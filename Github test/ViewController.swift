db3a26af5b94910b96709c23cbced9e7886665ab
Diana Rieser <114354@swcsd.us>
2021-04-16 12:56:12 -0400

    fristcomment

--- Github test/ViewController.swift
+++ Github test/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var whoIs = "talking in class"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print (whoIs)
     }
 
 
